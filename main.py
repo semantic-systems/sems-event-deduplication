451b34300716738274db540b4ad19cc08be801c3
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-09-14 15:03:24 +0200

    :sparkle: add aggregation code

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from simpletransformers.classification import ClassificationModel, ClassificationArgs
-import torch
+from sentence_transformers import SentenceTransformer, util
+import time
+import pandas as pd
+from pathlib import Path
 
-model_news_dup = ClassificationModel(
-    'roberta',
-    'vslaykovsky/roberta-news-duplicates',
-    use_cuda=torch.cuda.is_available(), 
-    args=ClassificationArgs(
-        fp16=True,
-        dataloader_num_workers=1,
-        use_multiprocessing_for_evaluation=False,
-        eval_batch_size=32,
-    )    
-) 
 
-print(model_news_dup.predict([
-    [
-        'Coronavirus: Third wave will "wash up on our shores", warns Johnson', 
-        'Boris Johnson has warned the effects of a third wave of coronavirus will "wash up on our shores" from Europe. The PM said the UK should be "under no illusion" we will "feel effects" of growing cases on the continent'
-    ]
-])[0])
+df = pd.read_csv("./data/gdelt_crawled/gdelt_crawled/storm/United States/aggregated_news.csv")
+df['start_date'] = pd.to_datetime(df['start_date'], format='%Y-%m-%d')
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+filtered_df = df.loc[(df['start_date'] >= '2021-01-03') & (df['start_date'] <= '2021-01-17')]
+print(filtered_df)
+corpus_embeddings = model.encode(filtered_df["title"].values, batch_size=64, show_progress_bar=True, convert_to_tensor=True)
+start_time = time.time()
+clusters = util.community_detection(corpus_embeddings, min_community_size=25, threshold=0.75)
+print("Clustering done after {:.2f} sec".format(time.time() - start_time))
+
+for i, cluster in enumerate(clusters):
+    print("\nCluster {}, #{} Elements ".format(i+1, len(cluster)))
+    cluster_sentences = list(set([filtered_df["title"].values[sentence_id] for sentence_id in cluster[0:]]))
+    for s in cluster_sentences:
+        print(f"  {s}")
