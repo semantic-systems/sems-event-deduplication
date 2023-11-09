8550bf1cd32fc4857b7b566a652500150c4b93f1
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-11-09 17:23:48 +0100

    :sparkles: add subclass of extraction

--- get_events.py
+++ get_events.py
@@ -12,17 +12,28 @@ Q_OCCURRENCE = "Q1190554"
 
 def instance_of_natural_disaster(item: WikidataItem) -> bool:
     claim_group = item.get_claim_group(P_INSTANCE_OF)
-    occupation_qids = [
+    disaster_qids = [
         claim.mainsnak.datavalue.value["id"]
         for claim in claim_group
         if claim.mainsnak.snaktype == "value"
     ]
-    return Q_NATURAL_DISASTER in occupation_qids
+    return Q_NATURAL_DISASTER in disaster_qids
+
+
+def subclass_of_natural_disaster(item: WikidataItem) -> bool:
+    claim_group = item.get_claim_group(P_SUBCLASS_OF)
+    disaster_qids = [
+        claim.mainsnak.datavalue.value["id"]
+        for claim in claim_group
+        if claim.mainsnak.snaktype == "value"
+    ]
+    return Q_NATURAL_DISASTER in disaster_qids
 
 
 # create an instance of WikidataJsonDump
 wjd_dump_path = "/data2/huang/latest-all.json.gz"
 wjd = WikidataJsonDump(wjd_dump_path)
+
 
 # create an iterable of WikidataItem representing politicians
 natural_disaster = []
@@ -30,7 +41,7 @@ t1 = time.time()
 for ii, entity_dict in enumerate(wjd):
     if entity_dict["type"] == "item":
         entity = WikidataItem(entity_dict)
-        if instance_of_natural_disaster(entity):
+        if instance_of_natural_disaster(entity) or subclass_of_natural_disaster(entity):
             natural_disaster.append(entity)
 
     if ii % 1000 == 0:
@@ -41,10 +52,9 @@ for ii, entity_dict in enumerate(wjd):
                 len(natural_disaster), ii, ii / dt
             )
         )
-        print(natural_disaster[-1])
+        if natural_disaster:
+            print(natural_disaster)
 
-    if ii > 10000:
-        break
 
 # write the iterable of WikidataItem to disk as JSON
 out_fname = "./filtered_natural_disaster_entities.json"
