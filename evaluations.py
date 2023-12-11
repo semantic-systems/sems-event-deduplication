c7539b437c06e4ea1f2f80cb9002db59d17fb422
Rudy Garrido Veliz <1garrido@informatik.uni-hamburg.de>
2023-12-11 14:43:55 +0100

    Working setup, bug for evaluations

--- evaluations.py
+++ evaluations.py
@@ -1,10 +1,49 @@
 import pandas as pd
 import torch
 import numpy as np
-from torch.utils.data import Dataset
+from datasets import Dataset
+from datasets.dataset_dict import DatasetDict
+from pytorch_lightning.loggers import WandbLogger
+
+import wandb
 from torch.utils.data import DataLoader
 from enum import Enum
-from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning import LightningModule  # , Trainer
+import evaluate
+from transformers import TrainingArguments, RobertaForSequenceClassification, AutoTokenizer, Trainer
+
+df = pd.read_csv("event_duration_prediction_dataset.csv")
+#df["regression_label"] = df["regression_label"].astype(float)
+label_enum = {k:j for j, k in enumerate(df['regression_label'].unique())}
+number_labels = len(label_enum)
+df['regression_label'] = df['regression_label'].apply(lambda x: [1.0 if label_enum[x]==i else 0.0 for i in range(number_labels)])
+max_length = df.apply(lambda x: len(x)).max()
+train_df = df.loc[df["event_type"].isin(["Hurricane Florence 2018", "Hurricane Sally 2020"])]
+valid_df = df.loc[df["event_type"].isin(["Hurricane Laura 2020", "Saddleridge Wildfire 2019"])]
+test_df = df.loc[df["event_type"].isin(
+    ["2018 Maryland Flood", "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"])]
+tokenizer = AutoTokenizer.from_pretrained("roberta-base", model_max_length=max_length)
+
+
+def tokenize_function(entry):
+    return tokenizer(entry["text"], padding="max_length", truncation=True)
+
+
+d = {'train': Dataset.from_dict(
+    {'text': train_df['text'].values.tolist(), 'label': train_df['regression_label'].values.tolist()}),
+    'test': Dataset.from_dict(
+        {'text': test_df['text'].values.tolist(), 'label': test_df['regression_label'].values.tolist()}),
+    'validation': Dataset.from_dict(
+        {'text': valid_df['text'].values.tolist(), 'label': valid_df['regression_label'].values.tolist()})
+}
+
+dataset = DatasetDict(d)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+train_dataset = tokenized_datasets["train"].shuffle(seed=42)
+test_dataset = tokenized_datasets["test"].shuffle(seed=42)
+eval_dataset = tokenized_datasets["validation"].shuffle(seed=42)
 
 
 # Make simple Enum for code clarity
@@ -14,86 +53,81 @@ class DatasetType(Enum):
     VAL = 3
 
 
-# Again create a Dataset but this time, do the split in train test val
-class CrisisfactsTestSet(Dataset):
-    event_dict = {"001": "Lilac Wildfire 2017",
-                  "002": "Cranston Wildfire 2018",
-                  "003": "Holy Wildfire 2018",
-                  "004": "Hurricane Florence 2018",
-                  "005": "2018 Maryland Flood",
-                  "006": "Saddleridge Wildfire 2019",
-                  "007": "Hurricane Laura 2020",
-                  "008": "Hurricane Sally 2020"}
-
-    def __init__(self):
-        # load data and shuffle, befor splitting
-        self.df = pd.read_csv("./data/test_from_crisisfacts.csv")
-        self.train_df = self.df.loc[self.df["event_type"].isin(["Hurricane Florence 2018", "Hurricane Sally 2020"])]
-        self.valid_df = self.df.loc[self.df["event_type"].isin(["Hurricane Laura 2020", "Saddleridge Wildfire 2019"])]
-        self.test_df = self.df.loc[self.df["event_type"].isin(["2018 Maryland Flood", "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"])]
-
-        self.train = self.train_df["text"]
-        self.val = self.valid_df["text"]
-        self.test = self.test_df["text"]
-
-        self.train_labels = self.train_df["event_type"]
-        self.val_labels = self.valid_df["event_type"]
-        self.test_labels = self.test_df["event_type"]
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        return self.dataset[idx], self.labels[idx]
-
-    def set_fold(self, set_type):
-        # Make sure to call this befor using the dataset
-        if set_type == DatasetType.TRAIN:
-            self.dataset, self.labels = self.train, self.train_labels
-        if set_type == DatasetType.TEST:
-            self.dataset, self.labels = self.test, self.test_labels
-        if set_type == DatasetType.VAL:
-            self.dataset, self.labels = self.val, self.val_labels
-        return self
+f1_metric = evaluate.load("f1")
+recall_metric = evaluate.load("recall")
+precision_metric = evaluate.load("precision")
+accuracy_metric = evaluate.load("accuracy")
 
 
-class EventDurationPredictionDataset(Dataset):
-    event_dict = {"001": "Lilac Wildfire 2017",
-                  "002": "Cranston Wildfire 2018",
-                  "003": "Holy Wildfire 2018",
-                  "004": "Hurricane Florence 2018",
-                  "005": "2018 Maryland Flood",
-                  "006": "Saddleridge Wildfire 2019",
-                  "007": "Hurricane Laura 2020",
-                  "008": "Hurricane Sally 2020"}
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=-1)
 
-    def __init__(self):
-        # load data and shuffle, befor splitting
-        self.df = pd.read_csv("./data/test_from_crisisfacts.csv")
-        self.train_df = self.df.loc[self.df["event_type"].isin(["Hurricane Florence 2018", "Hurricane Sally 2020"])]
-        self.valid_df = self.df.loc[self.df["event_type"].isin(["Hurricane Laura 2020", "Saddleridge Wildfire 2019"])]
-        self.test_df = self.df.loc[self.df["event_type"].isin(["2018 Maryland Flood", "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"])]
+    results = {}
+    results.update(f1_metric.compute(predictions=predictions, references=labels))
+    results.update(recall_metric.compute(predictions=predictions, references=labels))
+    results.update(precision_metric.compute(predictions=predictions, references=labels))
+    results.update(accuracy_metric.compute(predictions=predictions, references=labels))
 
-        self.train = self.train_df["text"]
-        self.val = self.valid_df["text"]
-        self.test = self.test_df["text"]
+    return results
 
-        self.train_labels = self.train_df["regression_label"]
-        self.val_labels = self.valid_df["regression_label"]
-        self.test_labels = self.test_df["regression_label"]
 
-    def __len__(self):
-        return len(self.dataset)
 
-    def __getitem__(self, idx):
-        return self.dataset[idx], self.labels[idx]
+class TrainerTransformer(LightningModule):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.model = RobertaForSequenceClassification.from_pretrained("roberta-base",
+                                                                      num_labels=vocab_size)  # .to("cuda")
 
-    def set_fold(self, set_type):
-        # Make sure to call this befor using the dataset
-        if set_type == DatasetType.TRAIN:
-            self.dataset, self.labels = self.train, self.train_labels
-        if set_type == DatasetType.TEST:
-            self.dataset, self.labels = self.test, self.test_labels
-        if set_type == DatasetType.VAL:
-            self.dataset, self.labels = self.val, self.val_labels
-        return self
+    def forward(self, inputs, target):
+        return self.model(inputs, target)
+
+    def training_step(self, batch, batch_idx):
+        inputs = torch.tensor(batch["text"], dtype=torch.long)
+        target = batch["label"]
+        output = self(inputs, target)
+        loss = torch.nn.functional.mse_loss(output, target.view(-1))
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.model.parameters(), lr=0.1)
+
+
+'''
+
+model = TrainerTransformer(number_labels)
+wandb_logger = WandbLogger(log_model="all")
+dataloader = DataLoader(train_dataset)
+trainer = Trainer(#logger=wandb_logger,
+                  max_epochs=3)
+trainer.fit(model=model, train_dataloaders=dataloader)
+'''
+
+model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=number_labels).to("cuda")
+# print(model.config)
+
+
+training_args = TrainingArguments(
+    output_dir="model-training",
+    evaluation_strategy="epoch",
+    learning_rate=7.121996518301633e-05,
+    num_train_epochs=3,
+    seed=38
+)
+
+trainer = Trainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+print('---------------------------------------------------------------------------------------------')
+
+# evaluate the model on the test set and print results
+pred = trainer.predict(test_dataset)
+print(pred.metrics)
