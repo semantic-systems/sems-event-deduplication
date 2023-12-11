66c106005b1231c2d12085ac745a44127a233e09
Rudy Garrido Veliz <1garrido@informatik.uni-hamburg.de>
2023-12-11 16:25:46 +0100

    Working training with Roberta, smaller dataset

--- evaluations.py
+++ evaluations.py
@@ -11,6 +11,7 @@ from enum import Enum
 from pytorch_lightning import LightningModule  # , Trainer
 import evaluate
 from transformers import TrainingArguments, RobertaForSequenceClassification, AutoTokenizer, Trainer
+from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv("event_duration_prediction_dataset.csv")
 #df["regression_label"] = df["regression_label"].astype(float)
@@ -18,10 +19,16 @@ label_enum = {k:j for j, k in enumerate(df['regression_label'].unique())}
 number_labels = len(label_enum)
 df['regression_label'] = df['regression_label'].apply(lambda x: [1.0 if label_enum[x]==i else 0.0 for i in range(number_labels)])
 max_length = df.apply(lambda x: len(x)).max()
-train_df = df.loc[df["event_type"].isin(["Hurricane Florence 2018", "Hurricane Sally 2020"])]
-valid_df = df.loc[df["event_type"].isin(["Hurricane Laura 2020", "Saddleridge Wildfire 2019"])]
+train_df = df.loc[df["event_type"].isin(["Hurricane Florence 2018"
+						#, "Hurricane Sally 2020"
+					])]
+valid_df = df.loc[df["event_type"].isin(["Hurricane Laura 2020"
+						#, "Saddleridge Wildfire 2019"
+					])]
 test_df = df.loc[df["event_type"].isin(
-    ["2018 Maryland Flood", "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"])]
+    ["2018 Maryland Flood"
+	#, "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"
+	])]
 tokenizer = AutoTokenizer.from_pretrained("roberta-base", model_max_length=max_length)
 
 
@@ -59,7 +66,9 @@ precision_metric = evaluate.load("precision")
 accuracy_metric = evaluate.load("accuracy")
 
 
+
 def compute_metrics(eval_pred):
+    '''
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=-1)
 
@@ -70,7 +79,10 @@ def compute_metrics(eval_pred):
     results.update(accuracy_metric.compute(predictions=predictions, references=labels))
 
     return results
-
+    '''
+    predictions, labels = eval_pred
+    rmse = mean_squared_error(labels, predictions, squared=False)
+    return {"rmse": rmse}
 
 
 class TrainerTransformer(LightningModule):
@@ -83,8 +95,7 @@ class TrainerTransformer(LightningModule):
         return self.model(inputs, target)
 
     def training_step(self, batch, batch_idx):
-        inputs = torch.tensor(batch["text"], dtype=torch.long)
-        target = batch["label"]
+        inputs, target = batch
         output = self(inputs, target)
         loss = torch.nn.functional.mse_loss(output, target.view(-1))
         return loss
@@ -103,16 +114,13 @@ trainer = Trainer(#logger=wandb_logger,
 trainer.fit(model=model, train_dataloaders=dataloader)
 '''
 
-model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=number_labels).to("cuda")
-# print(model.config)
+model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=number_labels, problem_type = 'regression').to("cuda")
 
 
 training_args = TrainingArguments(
     output_dir="model-training",
     evaluation_strategy="epoch",
-    learning_rate=7.121996518301633e-05,
-    num_train_epochs=3,
-    seed=38
+    num_train_epochs=1
 )
 
 trainer = Trainer(
@@ -121,13 +129,12 @@ trainer = Trainer(
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics
 )
 
 trainer.train()
 
 print('---------------------------------------------------------------------------------------------')
 
-# evaluate the model on the test set and print results
 pred = trainer.predict(test_dataset)
 print(pred.metrics)
