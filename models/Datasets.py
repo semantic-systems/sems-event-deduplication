36df06204dfa4ef2a2777f4ecd37f5d76685037a
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-01-17 16:36:22 +0100

    :sparkles: update data directory structure and seperate tasks.

--- models/Datasets.py
+++ models/Datasets.py
@@ -1,5 +1,6 @@
 import json
 from collections import Counter
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -9,23 +10,68 @@ from torch.utils.data import Dataset
 from itertools import combinations, chain
 import pickle
 import random
+from sklearn.model_selection import train_test_split
+
+
+def split_stormy_dataset():
+    df = pd.read_csv("./data/stormy_data/final_df_v2.csv")
+    unique_clusters = df.new_cluster.value_counts()
+    large_clusters = {i: c for i, c in unique_clusters.items() if c > 500}
+    medium_clusters = {i: c for i, c in unique_clusters.items() if 100 <= c <= 500}
+    small_clusters = {i: c for i, c in unique_clusters.items() if c <= 100}
+    train_index_large, test_index_large = train_test_split(list(large_clusters.keys()), test_size=0.2, random_state=420)
+    valid_index_large, test_index_large = train_test_split(test_index_large, test_size=0.5, random_state=420)
+    train_index_medium, test_index_medium = train_test_split(list(medium_clusters.keys()), test_size=0.3,
+                                                             random_state=420)
+    valid_index_medium, test_index_medium = train_test_split(test_index_medium, test_size=0.5, random_state=420)
+    train_index_small, test_index_small = train_test_split(list(small_clusters.keys()), test_size=0.4, random_state=420)
+    valid_index_small, test_index_small = train_test_split(test_index_small, test_size=0.6, random_state=420)
+    print(
+        f"large_clusters  {len(large_clusters)}] - train {len(train_index_large)} (sum: {sum([v for k, v in large_clusters.items() if k in train_index_large])}) - valid {len(valid_index_large)} (sum: {sum([v for k, v in large_clusters.items() if k in valid_index_large])}) - test {len(test_index_large)} (sum: {sum([v for k, v in large_clusters.items() if k in test_index_large])})")
+    print(
+        f"medium_clusters  {len(medium_clusters)}] - train {len(train_index_medium)} (sum: {sum([v for k, v in medium_clusters.items() if k in train_index_medium])}) - valid {len(valid_index_medium)} (sum: {sum([v for k, v in medium_clusters.items() if k in valid_index_medium])}) - test {len(test_index_medium)} (sum: {sum([v for k, v in medium_clusters.items() if k in test_index_medium])})")
+    print(
+        f"small_clusters  {len(small_clusters)}] - train {len(train_index_small)} (sum: {sum([v for k, v in small_clusters.items() if k in train_index_small])}) - valid {len(valid_index_small)} (sum: {sum([v for k, v in small_clusters.items() if k in valid_index_small])})- test {len(test_index_small)} (sum: {sum([v for k, v in small_clusters.items() if k in test_index_small])})")
+    df_train = df.loc[df["new_cluster"].isin(train_index_large + train_index_medium + train_index_small)]
+    df_valid = df.loc[df["new_cluster"].isin(valid_index_large + valid_index_medium + valid_index_small)]
+    df_test = df.loc[df["new_cluster"].isin(test_index_large + test_index_medium + test_index_small)]
+    print(f"df_train (len: {len(df_train)}) - df_valid (len: {len(df_valid)}) - df_test (len: {len(df_test)})")
+    df_train.to_csv("./data/stormy_data/train_v2.csv", index=False)
+    df_valid.to_csv("./data/stormy_data/valid_v2.csv", index=False)
+    df_test.to_csv("./data/stormy_data/test_v2.csv", index=False)
+
+
+def split_crisisfacts_dataset():
+    df = pd.read_csv("./data/crisisfacts_data/test_from_crisisfacts.csv")
+    train_df = df.loc[df["event_type"].isin(["Hurricane Florence 2018", "Hurricane Sally 2020"])]
+    valid_df = df.loc[df["event_type"].isin(["Hurricane Laura 2020", "Saddleridge Wildfire 2019"])]
+    test_df = df.loc[df["event_type"].isin(
+        ["2018 Maryland Flood", "Lilac Wildfire 2017", "Cranston Wildfire 2018", "Holy Wildfire 2018"])]
+    train_df.to_csv("./data/crisisfacts_data/crisisfacts_train.csv", index=False)
+    valid_df.to_csv("./data/crisisfacts_data/crisisfacts_valid.csv", index=False)
+    test_df.to_csv("./data/crisisfacts_data/crisisfacts_test.csv", index=False)
 
 
 class StormyDataset(torch.utils.data.Dataset):
-    def __init__(self, csv_path, label_pkl=None, sample_indices_path=None, subset: float = 1.0):
+    def __init__(self, csv_path, label_pkl=None, sample_indices_path=None, subset: float = 1.0, task: str = "combined"):
         random.seed(4)
+        self.task = task
         self.df = pd.read_csv(csv_path)
-        self.label2int = {"different_event": 0, "earlier": 1, "same_date": 2, "later": 3}
+        self.label2int = self.get_label2int(task)
         self.sentence_pairs_indices = list(combinations(range(len(self.df)), 2))
-        if label_pkl is not None:
+        if label_pkl is not None and Path(label_pkl).exists():
             with open(label_pkl, "rb") as fp:
                 self.labels = pickle.load(fp)
         else:
             self.labels = list(map(self.get_label, self.sentence_pairs_indices))
+            if "ignored" in self.labels:
+                self.valid_label_indices = [i for i, label in enumerate(self.labels) if label != "ignored"]
+                self.labels = [self.labels[i] for i in self.valid_label_indices]
+                self.sentence_pairs_indices = [self.sentence_pairs_indices[i] for i in self.valid_label_indices]
 
         if sample_indices_path:
-            self.sample_indices = self.get_sample_indices(self.labels, sample_indices_path)
-            self.labels = self.get_balanced_labels(sample_indices_path)
+            sample_indices = self.get_sample_indices(self.labels, sample_indices_path)
+            self.labels = self.get_balanced_labels(sample_indices)
         self.labels = [self.label2int[label] for label in self.labels]
 
         self.sentence_pairs_indices = self.get_sentence_pairs_indices(sample_indices_path=sample_indices_path)
@@ -33,6 +79,18 @@ class StormyDataset(torch.utils.data.Dataset):
         self.sentence_pairs_indices = self.sentence_pairs_indices[:self.end_index]
 
         self.get_descriptions()
+
+    @staticmethod
+    def get_label2int(task):
+        if task == "combined":
+            label2int = {"different_event": 0, "earlier": 1, "same_date": 2, "later": 3}
+        elif task == "event_deduplication":
+            label2int = {"different_event": 0, "same_event": 1}
+        elif task == "event_temporality":
+            label2int = {"earlier": 0, "same_date": 1, "later": 2}
+        else:
+            ValueError(f"{task} not defined! Please choose from 'combined', 'event_deduplication' or 'event_temporality'")
+        return label2int
 
     def get_sentence_pairs_indices(self, sample_indices_path: str = None):
         if not sample_indices_path:
@@ -48,6 +106,7 @@ class StormyDataset(torch.utils.data.Dataset):
     @staticmethod
     def get_sample_indices(labels, save_path):
         if not Path(save_path).exists():
+            print("Creating sample indices to balance dataset...")
             c = Counter(labels)
             sample_probabilities = {key: min(c.values())/value for key, value in c.items()}
             print(save_path)
@@ -67,29 +126,47 @@ class StormyDataset(torch.utils.data.Dataset):
                 sample_indices = json.load(f)
             return sample_indices
 
-    def get_balanced_labels(self, sample_indices_path: str = None):
-        with open(sample_indices_path, 'r') as f:
-            sample_indices = json.load(f)
+    def get_balanced_labels(self, sample_indices):
         sample_indices = list(chain(*list(sample_indices.values())))
         return [self.labels[i] for i in sample_indices]
 
     def get_label(self, index_tuple):
         clusters = self.df.new_cluster.values
-        unix_times = self.df.normalized_date.values
         cluster_i = clusters[index_tuple[0]]
         cluster_j = clusters[index_tuple[1]]
-        if cluster_i != cluster_j:
-            return "different_event"
-        else:
-            unix_time_i = unix_times[index_tuple[0]]
-            unix_time_j = unix_times[index_tuple[1]]
-            if unix_time_i < unix_time_j:
-                label = "earlier"
-            elif unix_time_i == unix_time_j:
-                label = "same_date"
+        if self.task == "combined":
+            if cluster_i != cluster_j:
+                return "different_event"
             else:
-                label = "later"
-            return label
+                unix_times = self.df.normalized_date.values
+                unix_time_i = unix_times[index_tuple[0]]
+                unix_time_j = unix_times[index_tuple[1]]
+                if unix_time_i < unix_time_j:
+                    label = "earlier"
+                elif unix_time_i == unix_time_j:
+                    label = "same_date"
+                else:
+                    label = "later"
+                return label
+        elif self.task == "event_deduplication":
+            if cluster_i != cluster_j:
+                return "different_event"
+            else:
+                return "same_event"
+        elif self.task == "event_temporality":
+            if cluster_i != cluster_j:
+                return "ignored"
+            else:
+                unix_times = self.df.normalized_date.values
+                unix_time_i = unix_times[index_tuple[0]]
+                unix_time_j = unix_times[index_tuple[1]]
+                if unix_time_i < unix_time_j:
+                    label = "earlier"
+                elif unix_time_i == unix_time_j:
+                    label = "same_date"
+                else:
+                    label = "later"
+                return label
 
     def get_descriptions(self):
         print(f"The dataset csv has {len(self.df)} entries with the following columns - {self.df.columns.values}")
@@ -105,22 +182,26 @@ class StormyDataset(torch.utils.data.Dataset):
 
 
 class CrisisFactsDataset(StormyDataset):
-    def __init__(self, csv_path, label_pkl=None, sample_indices_path=None, subset: float = 1.0):
-        super().__init__(csv_path, label_pkl, sample_indices_path, subset)
+    def __init__(self, csv_path, label_pkl=None, sample_indices_path=None, subset: float = 1.0, task: str = "combined"):
+        super().__init__(csv_path, label_pkl, sample_indices_path, subset, task)
         random.seed(4)
         self.df = pd.read_csv(csv_path)
         self.df.rename({'text': 'title'}, axis=1, inplace=True)
-        self.label2int = {"different_event": 0, "earlier": 1, "same_date": 2, "later": 3}
+        self.label2int = self.get_label2int(task)
         self.sentence_pairs_indices = list(combinations(range(len(self.df)), 2))
-        if label_pkl is not None:
+        if label_pkl is not None and Path(label_pkl).exists():
             with open(label_pkl, "rb") as fp:
                 self.labels = pickle.load(fp)
         else:
             self.labels = list(map(self.get_label, self.sentence_pairs_indices))
+            if "ignored" in self.labels:
+                self.valid_label_indices = [i for i, label in enumerate(self.labels) if label != "ignored"]
+                self.labels = [self.labels[i] for i in self.valid_label_indices]
+                self.sentence_pairs_indices = [self.sentence_pairs_indices[i] for i in self.valid_label_indices]
 
         if sample_indices_path:
-            self.sample_indices = self.get_sample_indices(self.labels, sample_indices_path)
-            self.labels = self.get_balanced_labels(sample_indices_path)
+            sample_indices = self.get_sample_indices(self.labels, sample_indices_path)
+            self.labels = self.get_balanced_labels(sample_indices)
         self.labels = [self.label2int[label] for label in self.labels]
 
         self.sentence_pairs_indices = self.get_sentence_pairs_indices(sample_indices_path=sample_indices_path)
@@ -134,18 +215,44 @@ class CrisisFactsDataset(StormyDataset):
         unix_times = self.df.unix_timestamp.values
         cluster_i = clusters[index_tuple[0]]
         cluster_j = clusters[index_tuple[1]]
-        if cluster_i != cluster_j:
-            return "different_event"
-        else:
-            unix_time_i = unix_times[index_tuple[0]]
-            unix_time_j = unix_times[index_tuple[1]]
-            if unix_time_i < unix_time_j:
-                label = "earlier"
-            elif unix_time_i == unix_time_j:
-                label = "same_date"
+        if self.task == "combined":
+            if cluster_i != cluster_j:
+                return "different_event"
             else:
-                label = "later"
-            return label
+                unix_time_i = self.round_to_day(unix_times[index_tuple[0]])
+                unix_time_j = self.round_to_day(unix_times[index_tuple[1]])
+                if unix_time_i < unix_time_j:
+                    label = "earlier"
+                elif unix_time_i == unix_time_j:
+                    label = "same_date"
+                else:
+                    label = "later"
+                return label
+        elif self.task == "event_deduplication":
+            if cluster_i != cluster_j:
+                return "different_event"
+            else:
+                return "same_event"
+        elif self.task == "event_temporality":
+            if cluster_i != cluster_j:
+                return "ignored"
+            else:
+                unix_time_i = self.round_to_day(unix_times[index_tuple[0]])
+                unix_time_j = self.round_to_day(unix_times[index_tuple[1]])
+                if unix_time_i < unix_time_j:
+                    label = "earlier"
+                elif unix_time_i == unix_time_j:
+                    label = "same_date"
+                else:
+                    label = "later"
+                return label
+
+    @staticmethod
+    def round_to_day(timestamp):
+        dt_object = datetime.utcfromtimestamp(timestamp)
+        rounded_dt = dt_object.replace(hour=0, minute=0, second=0, microsecond=0)
+        rounded_timestamp = int(rounded_dt.timestamp())
+        return rounded_timestamp
 
     def get_descriptions(self):
         print(f"The dataset csv has {len(self.df)} entries with the following columns - {self.df.columns.values}")
@@ -161,13 +268,13 @@ class CrisisFactsDataset(StormyDataset):
 
 
 if __name__ == "__main__":
-    train_csv_path = Path("./data/gdelt_crawled/train_v1.csv")
-    valid_csv_path = Path("./data/gdelt_crawled/valid_v1.csv")
-    test_csv_path = Path("./data/gdelt_crawled/test_v1.csv")
-    test_crisisfacts_csv_path = Path("./data/test_from_crisisfacts.csv")
-    # train = StormyDataset(train_csv_path, label_pkl=Path("../data/gdelt_crawled/labels_train.pkl"))
-    # valid = StormyDataset(valid_csv_path, label_pkl=Path("../data/gdelt_crawled/labels_valid.pkl"))
-    # test = StormyDataset(test_csv_path, label_pkl=Path("../data/gdelt_crawled/labels_test.pkl"))
+    train_csv_path = Path("./data/stormy_data/train_v1.csv")
+    valid_csv_path = Path("./data/stormy_data/valid_v1.csv")
+    test_csv_path = Path("./data/stormy_data/test_v1.csv")
+    test_crisisfacts_csv_path = Path("./data/crisisfacts_data/test_from_crisisfacts.csv")
+    # train = StormyDataset(train_csv_path, label_pkl=Path("../data/stormy_data/labels_train.pkl"))
+    # valid = StormyDataset(valid_csv_path, label_pkl=Path("../data/stormy_data/labels_valid.pkl"))
+    # test = StormyDataset(test_csv_path, label_pkl=Path("../data/stormy_data/labels_test.pkl"))
     test_crisisfacts = CrisisFactsDataset(test_crisisfacts_csv_path, label_pkl=None)
     # print(f"Training data \n {train.get_descriptions()}\n")
     # print(f"Validation data \n {valid.get_descriptions()}\n")
--- models/EventPairwiseTemporality.py
+++ models/EventPairwiseTemporality.py
@@ -17,13 +17,17 @@ logging.getLogger().setLevel(logging.INFO)
 class EventPairwiseTemporalityModel(object):
     def __init__(self,
                  batch_size: int = 512,
+                 num_epochs: int = 10,
                  exp_name: str = "v1",
                  transformer_model: str = 'distilbert-base-uncased',
                  subset: float = 1.0,
-                 load_pretrained: bool = False):
+                 load_pretrained: bool = False,
+                 task: str = "combined"):
         self.exp_name = exp_name
+        self.num_epochs = num_epochs
+        self.task = task  # task in {"combined", "event_deduplication", "event_temporality"}
         self.subset = subset
-        self.prepare_environment(exp_name)
+        self.prepare_environment(exp_name, task)
         self.batch_size = batch_size
         word_embedding_model = models.Transformer(transformer_model, max_seq_length=256)
         pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
@@ -32,7 +36,7 @@ class EventPairwiseTemporalityModel(object):
         if not load_pretrained:
             self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model, dense_model], device=self.device)
         else:
-            self.model = SentenceTransformer(str(Path("./outputs", exp_name).absolute()))
+            self.model = SentenceTransformer(str(Path("./outputs", exp_name, task).absolute()))
 
         self.label2int = {"different_event": 0, "earlier": 1, "same_date": 2, "later": 3}
 
@@ -45,18 +49,18 @@ class EventPairwiseTemporalityModel(object):
         validation_dataset = SentencesDataset(validation_data, self.model)
         self.training_dataloader = DataLoader(training_dataset, shuffle=True, batch_size=self.batch_size)
         self.validation_dataloader = DataLoader(validation_dataset, shuffle=True, batch_size=self.batch_size)
-        self.validation_evaluator = EventPairwiseTemporalityEvaluator(self.validation_dataloader, name='validation_'+exp_name, softmax_model=self.train_loss)
+        self.validation_evaluator = EventPairwiseTemporalityEvaluator(self.validation_dataloader, name=f'validation_{exp_name}_{task}', softmax_model=self.train_loss)
 
     def prepare_data(self, test=False):
         if not test:
-            train_csv_path = Path("./data/gdelt_crawled/train_v2.csv")
-            valid_csv_path = Path("./data/gdelt_crawled/valid_v2.csv")
-            train = StormyDataset(train_csv_path, label_pkl=Path("./data/gdelt_crawled/labels_train.pkl"),
-                                  sample_indices_path=Path("./data/gdelt_crawled/sample_indices_train.json"),
-                                  subset=self.subset)
-            valid = StormyDataset(valid_csv_path, label_pkl=Path("./data/gdelt_crawled/labels_valid.pkl"),
-                                  sample_indices_path=Path("./data/gdelt_crawled/sample_indices_valid.json"),
-                                  subset=self.subset)
+            train_csv_path = Path("./data/stormy_data/train_v2.csv")
+            valid_csv_path = Path("./data/stormy_data/valid_v2.csv")
+            train = StormyDataset(train_csv_path, label_pkl=Path(f"./data/stormy_data/{self.task}/labels_train.pkl"),
+                                  sample_indices_path=Path(f"./data/stormy_data/{self.task}/sample_indices_train.json"),
+                                  subset=self.subset, task=self.task)
+            valid = StormyDataset(valid_csv_path, label_pkl=Path(f"./data/stormy_data/{self.task}/labels_valid.pkl"),
+                                  sample_indices_path=Path(f"./data/stormy_data/{self.task}/sample_indices_valid.json"),
+                                  subset=self.subset, task=self.task)
             train_labels = train.labels
             train_titles = train.df["title"].values
             valid_labels = valid.labels
@@ -72,11 +76,11 @@ class EventPairwiseTemporalityModel(object):
 
             return train_examples, valid_examples
         else:
-            test_csv_path = Path("./data/gdelt_crawled/test_v2.csv")
+            test_csv_path = Path(f"./data/stormy_data/test_v2.csv")
             test = StormyDataset(test_csv_path,
-                                 label_pkl=Path("./data/gdelt_crawled/labels_test.pkl"),
-                                 sample_indices_path=Path("./data/gdelt_crawled/sample_indices_test.json"),
-                                 subset=self.subset)
+                                 label_pkl=Path(f"./data/stormy_data/{self.task}/labels_test.pkl"),
+                                 sample_indices_path=Path(f"./data/stormy_data/{self.task}/sample_indices_test.json"),
+                                 subset=self.subset, task=self.task)
             test_labels = test.labels
             test_titles = test.df["title"].values
             test_examples = [InputExample(texts=[test_titles[test.sentence_pairs_indices[i][0]],
@@ -84,11 +88,11 @@ class EventPairwiseTemporalityModel(object):
                                           label=test_labels[i]) for i in range(len(test))]
             logger.info(f"Test: {len(test_examples)} pairs of sentences")
 
-            test_csv_path = Path("./data/test_from_crisisfacts.csv")
+            test_csv_path = Path("./data/crisisfacts_data/test_from_crisisfacts.csv")
             test = CrisisFactsDataset(test_csv_path,
-                                      label_pkl=Path("./data/gdelt_crawled/labels_crisisfacts_test.pkl"),
-                                      sample_indices_path=Path("./data/gdelt_crawled/sample_indices_test_crisisfacts.json"),
-                                      subset=self.subset)
+                                      label_pkl=Path(f"./data/crisisfacts_data/{self.task}/labels_crisisfacts_test.pkl"),
+                                      sample_indices_path=Path(f"./data/crisisfacts_data/{self.task}/sample_indices_test_crisisfacts.json"),
+                                      subset=self.subset, task=self.task)
             test_labels = test.labels
             test_titles = test.df["title"].values
             test_examples_crisisfact = [InputExample(texts=[test_titles[test.sentence_pairs_indices[i][0]],
@@ -98,23 +102,21 @@ class EventPairwiseTemporalityModel(object):
             return test_examples, test_examples_crisisfact
 
     def train(self):
-        # Configure the training
-        num_epochs = 10
 
-        warmup_steps = math.ceil(len(self.training_dataloader) * num_epochs * 0.1)  # 10% of train data for warm-up
+        warmup_steps = math.ceil(len(self.training_dataloader) * self.num_epochs * 0.1)  # 10% of train data for warm-up
         logger.info(f"Warmup-steps: {warmup_steps}")
-        logger.info(f"Number of epochs: {num_epochs}")
-        logger.info(f"Output path: {str(Path('./outputs', self.exp_name))}")
+        logger.info(f"Number of epochs: {self.num_epochs}")
+        logger.info(f"Output path: {str(Path('./outputs', self.exp_name, self.task))}")
 
-        self.validation_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name)))
+        self.validation_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name, self.task)))
 
         # Train the model
         self.model.fit(train_objectives=[(self.training_dataloader, self.train_loss)],
                        evaluator=self.validation_evaluator,
-                       epochs=num_epochs,
+                       epochs=self.num_epochs,
                        evaluation_steps=10000,
                        warmup_steps=warmup_steps,
-                       output_path=str(Path("./outputs", self.exp_name)),
+                       output_path=str(Path("./outputs", self.exp_name, self.task)),
                        show_progress_bar=True,
                        save_best_model=True
                       )
@@ -124,27 +126,30 @@ class EventPairwiseTemporalityModel(object):
         testing_data, testing_data_crisisfacts = self.prepare_data(test=True)
         testing_dataset = SentencesDataset(testing_data, self.model)
         testing_dataloader = DataLoader(testing_dataset, shuffle=True, batch_size=self.batch_size)
-        testing_evaluator = EventPairwiseTemporalityEvaluator(testing_dataloader, name='test_' + self.exp_name,
-                                                                   softmax_model=self.train_loss)
+        testing_evaluator = EventPairwiseTemporalityEvaluator(testing_dataloader,
+                                                              name=f'test_{self.exp_name}_{self.task}',
+                                                              softmax_model=self.train_loss)
 
-        testing_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name)))
+        testing_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name, self.task)))
 
 
         logger.info(f"Testing on Crisisfacts test set.")
         testing_dataset = SentencesDataset(testing_data_crisisfacts, self.model)
         testing_dataloader = DataLoader(testing_dataset, shuffle=True, batch_size=self.batch_size)
-        testing_evaluator = EventPairwiseTemporalityEvaluator(testing_dataloader, name='test_crisisfacts_' + self.exp_name,
+        testing_evaluator = EventPairwiseTemporalityEvaluator(testing_dataloader,
+                                                              name=f'test_crisisfacts_{self.exp_name}_{self.task}',
                                                               softmax_model=self.train_loss)
 
-        testing_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name)))
+        testing_evaluator(self.model, output_path=str(Path("./outputs", self.exp_name, self.task)))
 
     @staticmethod
-    def prepare_environment(exp_name):
+    def prepare_environment(exp_name, task):
         if not Path("./outputs").exists():
             Path("./outputs").mkdir()
         if not Path("./outputs", exp_name).exists():
             Path("./outputs", exp_name).mkdir()
-
+        if not Path("./outputs", exp_name, task).exists():
+            Path("./outputs", exp_name, task).mkdir()
     @property
     def device(self):
         if torch.cuda.is_available():
@@ -154,6 +159,12 @@ class EventPairwiseTemporalityModel(object):
 
 
 if __name__ == "__main__":
-    model = EventPairwiseTemporalityModel(batch_size=512, exp_name="v1", transformer_model='distilbert-base-uncased', subset=0.1, load_pretrained=True)
-    # model.train()
+    model = EventPairwiseTemporalityModel(batch_size=512,
+                                          num_epochs=2,
+                                          exp_name="test",
+                                          transformer_model='distilbert-base-uncased',
+                                          subset=0.01,
+                                          load_pretrained=True,
+                                          task="combined")
+    model.train()
     model.test()
