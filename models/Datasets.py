eb7080060d93090a502359aace65cf5b6abec0d8
Junbo Huang <junbo.huang@uni-hamburg.de>
2024-01-29 03:33:01 +0100

    :bug: balance between datasets

--- models/Datasets.py
+++ models/Datasets.py
@@ -72,8 +72,8 @@ def generate_diversified_random_pairs(df, multiplier, get_label):
         A_label_ignored_indics = [i for i, label in enumerate(A_label) if label == "ignored"]
         non_ignored_pairs = {i: a for i, a in enumerate(A) if i not in A_label_ignored_indics}
         sample_indics = list(non_ignored_pairs.keys())
-        pairs = [A[i] for i in sample_indics]
-        labels = [non_ignored_pairs[i] for i in sample_indics]
+        pairs.extend([A[i] for i in sample_indics])
+        labels.extend([A_label[i] for i in sample_indics])
     return pairs, labels
 
 
